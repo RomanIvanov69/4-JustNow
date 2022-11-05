a630c1a980424e733df0b9c81d22bca308ef2a11
Roman Ivanov <roman_ivanov85@internet.ru>
2022-11-05 17:28:39 +0300

    change main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,35 @@
 package ru.netology
 
 fun main() {
+    val timeLastVisit = 10_000
 
+    println("был(а) " + agoToText(timeLastVisit))
+}
+
+fun agoToText(timeLastVisit: Int): String {
+
+    return when {
+        timeLastVisit <= 60 -> "только что"
+        timeLastVisit in 61..(60 * 60) -> "${timeLastVisit / 60}" + minuteAgo(timeLastVisit / 60) + " назад"
+        timeLastVisit in (60 * 60 + 1)..(24 * 60 * 60) -> "${timeLastVisit / 3600}" + hoursAgo(timeLastVisit / 3600) + " назад"
+        timeLastVisit in (24 * 60 * 60 + 1)..(24 * 60 * 60 * 2) -> "вчера"
+        timeLastVisit in (24 * 60 * 60 * 2 + 1)..(24 * 60 * 60 * 3) -> "позавчера"
+        else -> "давно"
+    }
+}
+
+fun minuteAgo(minuteToText: Int): String {
+    return when (minuteToText) {
+        1, 21, 31, 41, 51 -> return " минуту"
+        2, 3, 4 -> return " минуты"
+        else -> return " минут"
+    }
+}
+
+fun hoursAgo(hoursToText: Int): String {
+    return when (hoursToText) {
+        1, 21 -> return " час"
+        2, 3, 4 -> return " часа"
+        else -> return " часов"
+    }
 }
